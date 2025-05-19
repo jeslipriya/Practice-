4b316e317d1f8191b0c9d0929750988d076f5d58
Jesli <jeslipriya07@gmail.com>
2025-05-19 12:01:28 +0530

    Added Pattern.cs

---
+++ CSharp/Pattern.cs
@@ -0,0 +1,25 @@
+﻿class Pattern
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Enter the number of rows: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the number of rows: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter any symbol");
+        string symbol = Console.ReadLine();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
